55d3a53cfbb351a900ef3e180d94b595cd8c6945
Ronald Heimert <r.heimert@gmail.com>
2023-06-22 10:50:37 +0200

    added AddDog button with styling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Heading from "../components/Heading";
 import DogList from "../components/DogList";
+import AddButton from "../components/AddButton";
 
 export default function Home({ dogs }) {
   return (
     <main>
       <Heading>Dog Pass 🐕‍🦺</Heading>
       <DogList dogs={dogs} />
+      <AddButton />
     </main>
   );
 }

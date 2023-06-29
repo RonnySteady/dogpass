24de84154ff8ec3daf2c718941853ccd6ddb4cd3
Ronald Heimert <r.heimert@gmail.com>
2023-06-29 23:46:54 +0200

    new commit after git conflict: added delete function with confirmation window alert

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,20 @@ import Heading from "../components/Heading";
 import DogList from "../components/DogList";
 import AddDogButton from "../components/AddDogButton";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home({ dogs }) {
+  const [dogList, setDogList] = useState(dogs);
+
+  const handleDeleteDog = (dogName) => {
+    const updatedDogs = dogList.filter((dog) => dog.name !== dogName);
+    setDogList(updatedDogs);
+  };
+
   return (
     <main>
       <Heading>Dog Pass 🐕‍🦺</Heading>
-      <DogList dogs={dogs} />
+      <DogList dogs={dogList} onDeleteDog={handleDeleteDog} />
       <Link href="/newdog">
         <AddDogButton />
       </Link>

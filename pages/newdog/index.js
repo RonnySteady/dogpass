9fd03db45a165c075f2a8bd2da190cc4341f6dd6
Ronny <Ronny@MacBook-Pro.local>
2023-07-15 12:18:31 +0200

    added NavBar to newdog-page

--- pages/newdog/index.js
+++ pages/newdog/index.js
@@ -1,11 +1,18 @@
 import Header from "../../components/Header";
 import AddDogForm from "../../components/AddDogForm";
+import NavBar from "../../components/NavBar";
 
 export default function DogForm({ dogs }) {
   return (
     <main>
       <Header />
       <AddDogForm dogs={dogs} />
+      <NavBar
+        buttonText1="Go to dogs"
+        link1="/"
+        buttonText2="Go to owner"
+        link2="/owner"
+      />
     </main>
   );
 }
